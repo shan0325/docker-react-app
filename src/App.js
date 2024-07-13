215ad353c262238418d13ddd0f86d0f841914ad4
shan0325 <shan0325@naver.com>
2024-07-13 17:39:02 +0900

    modified App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.11111
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React111
+          Learn React
         </a>
       </header>
     </div>
